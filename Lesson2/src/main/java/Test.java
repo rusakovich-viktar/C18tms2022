b437c85b1aaf1a6a39d22c6a1a26fa968fadd59c
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-10-25 20:47:05 +0300

    create bugfix

--- Lesson2/src/main/java/Test.java
+++ Lesson2/src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("change in develop branch");
+        System.out.println("change in develop>bugfix branch");
     }
 }
