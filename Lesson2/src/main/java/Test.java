952320f624ce5d7179652ee8fd3932b9f539da1f
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-10-25 20:36:32 +0300

    change sout msg in develop branch

--- Lesson2/src/main/java/Test.java
+++ Lesson2/src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Create in main branch");
+        System.out.println("change in develop branch");
     }
 }
