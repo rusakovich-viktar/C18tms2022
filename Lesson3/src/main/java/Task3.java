917f42d03aa7d152e098f8197c8723db4291ca45
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-10-26 12:32:04 +0300

    Add Task3 (without forEach)

---
+++ Lesson3/src/main/java/Task3.java
@@ -0,0 +1,31 @@
+/**
+ * //     * Метод должен вернуть среднее значение из массива чисел
+ * //     * (необходимо сумму всех элеменов массива разделить на длину массива)
+ * //     * <p>
+ * //     * Example:
+ * //     * array = {1,2,3,4,5}
+ * //     * Метод должен return 3.0
+ * //
+ */
+public class Task3 {
+    public static double average(int[] array) {
+        int sumArray = 0;
+        for (int i = 0; i < array.length; i++) {
+            sumArray += array[i];
+        }
+        if (sumArray == 0) {
+            return 0;
+        } else {
+            return (double) sumArray / array.length;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(new int[]{}));
+        System.out.println(average(new int[]{0}));
+        System.out.println(average(new int[]{0, 3}));
+        System.out.println(average(new int[]{0, 0, 3}));
+        System.out.println(average(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(average(new int[]{0, -2, 3, -1, 5}));
+    }
+}
