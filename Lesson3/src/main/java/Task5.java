b4551b97efe388e833f868f66e9e8d022841186c
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-10-26 21:57:34 +0300

    Add Task5

---
+++ Lesson3/src/main/java/Task5.java
@@ -0,0 +1,24 @@
+/**
+ * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
+ * <p>
+ * Example1:
+ * 3
+ * 4
+ * return 5
+ * <p>
+ * Example2:
+ * 12
+ * 16
+ * return 20
+ */
+
+public class Task5 {
+    public static int calculateHypotenuse(int a, int b) {
+        return (int) Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateHypotenuse(3, 4));
+        System.out.println(calculateHypotenuse(12, 16));
+    }
+}
