585cf635b4d2d86479624b675dadeec1800cfcfa
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-10-26 11:31:36 +0300

    Add task1

--- Lesson3/src/main/java/Task1.java
+++ Lesson3/src/main/java/Task1.java
@@ -1,3 +1,16 @@
 public class Task1 {
+    public static int sum(int a, int b) {
+        if ((long) a + (long) b > Integer.MAX_VALUE || (long) a + (long) b < Integer.MIN_VALUE) {
+            return -1;
+        } else {
+            return a + b;
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println(sum(100, 200));
+        System.out.println(sum(-100, -200));
+        System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        System.out.println(sum(Integer.MIN_VALUE, Integer.MIN_VALUE));
+    }
 }
