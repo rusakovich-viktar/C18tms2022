1210b243e8865ea1852e1bb32adb5cbdf978e322
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-11-20 14:05:24 +0300

    add task Computer

--- Lesson6/src/main/java/by/tms/service/Main.java
+++ Lesson6/src/main/java/by/tms/service/Main.java
@@ -4,7 +4,15 @@ import by.tms.model.Computer;
 
 public class Main {
     public static void main(String[] args) {
-        Computer computer = new Computer("RYZEN", 16, 256,5);
+        Computer computer = new Computer("RYZEN 7", 16, 256, 5);
+
+        for (int i = 0; i < computer.getResourcesFullCycles(); i++) {
+            if (computer.isBurned()) {
+                break;
+            }
+            computer.powerOn();
+            computer.powerOff();
+        }
         computer.specification();
     }
 
