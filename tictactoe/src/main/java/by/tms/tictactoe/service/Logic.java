b21910426f28ee2112573a0d299bb9bed1fce0be
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-11-24 19:59:42 +0300

    tictactoe logic add

--- tictactoe/src/main/java/by/tms/tictactoe/service/Logic.java
+++ tictactoe/src/main/java/by/tms/tictactoe/service/Logic.java
@@ -12,12 +12,28 @@ public class Logic {
 
     public boolean isWinnerX() {
         //реализовать логику по проверке, что 3 крестика в одной строке или столбце или по диагонали
-        return false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i][0].hasMarkX() && table[i][1].hasMarkX() && table[i][2].hasMarkX()) {
+                return true;
+            } else if (table[0][i].hasMarkX() && table[1][i].hasMarkX() && table[2][i].hasMarkX()) {
+            }
+        }
+        if (table[0][0].hasMarkX() && table[1][1].hasMarkX() && table[2][2].hasMarkX()) {
+            return true;
+        } else return table[2][2].hasMarkX() && table[1][1].hasMarkX() && table[2][0].hasMarkX();
     }
 
     public boolean isWinnerO() {
         //реализовать логику по проверке, что 3 нолика в одной строке или столбце или по диагонали
-        return false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i][0].hasMarkO() && table[i][1].hasMarkO() && table[i][2].hasMarkO()) {
+                return true;
+            } else if (table[0][i].hasMarkO() && table[1][i].hasMarkO() && table[2][i].hasMarkO()) {
+            }
+        }
+        if (table[0][0].hasMarkO() && table[1][1].hasMarkO() && table[2][2].hasMarkO()) {
+            return true;
+        } else return table[2][2].hasMarkO() && table[1][1].hasMarkO() && table[2][0].hasMarkO();
     }
 
     /**
