d5d6a6efc93f34bd7db9c9e9053863c7b366d4d4
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-11-27 00:41:44 +0300

    fix9 task "№;%:?"

--- Lesson8add/src/main/java/Task9.java
+++ Lesson8add/src/main/java/Task9.java
@@ -17,8 +17,11 @@ public class Task9 {
         String[][] chessBoard = new String[8][8];
         for (int i = 0; i < chessBoard.length; i++) {
             for (int j = 0; j < chessBoard[0].length; j++) {
-                if ((i + j) % 2 == 0) chessBoard[i][j] = "W";
-                else chessBoard[i][j] = "B";
+                if ((i + j) % 2 == 0) {
+                    chessBoard[i][j] = "W";
+                } else {
+                    chessBoard[i][j] = "B";
+                }
             }
         }
 
