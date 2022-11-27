997de47546031918f16d2ffd6b50ab35ab57f801
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-11-27 11:35:21 +0300

    fix (Test не складывается)

--- Lesson8add/src/main/java/Task9.java
+++ Lesson8add/src/main/java/Task9.java
@@ -16,7 +16,7 @@ public class Task9 {
     public static void main(String[] args) {
         String[][] chessBoard = new String[8][8];
         for (int i = 0; i < chessBoard.length; i++) {
-            for (int j = 0; j < chessBoard[0].length; j++) {
+            for (int j = 0; j < chessBoard.length; j++) {
                 if ((i + j) % 2 == 0) {
                     chessBoard[i][j] = "W";
                 } else {
@@ -32,6 +32,4 @@ public class Task9 {
             System.out.println();
         }
     }
-
-
 }
---
+++ Lesson8add/src/test/java/Task1Test.java
@@ -0,0 +1,7 @@
+class Task1Test {
+
+//    @Test
+//    void main() {
+//        Assertions.assertEquals(((new int[]{0, 1, 3, 0, 4})), Task1.removeElement((new int[]{0, 1, 2, 2, 3, 0, 4, 2}), 2));
+//    }
+}
