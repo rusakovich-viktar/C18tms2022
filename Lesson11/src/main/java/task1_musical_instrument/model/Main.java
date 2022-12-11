0340bfe6d4e7e510d4132222c871a733d5a01024
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-12-11 23:19:15 +0300

    add Lesson 11 Task1

---
+++ Lesson11/src/main/java/task1_musical_instrument/model/Main.java
@@ -0,0 +1,22 @@
+package task1_musical_instrument.model;
+
+public class Main {
+    public static void main(String[] args) {
+        IInstrument[] instruments = new IInstrument[IInstrument.Type.values().length];
+        instruments[0] = createInstrument(IInstrument.Type.GUITAR, 6);
+        instruments[1] = createInstrument(IInstrument.Type.DRUM, 24);
+        instruments[2] = createInstrument(IInstrument.Type.TUBE, 11);
+        for (IInstrument instrument : instruments) {
+            instrument.play();
+
+        }
+    }
+
+    private static IInstrument createInstrument(IInstrument.Type iType, int argument) { //почему без статик не видит в классе?
+        return switch (iType) {
+            case GUITAR -> new Guitar(argument);
+            case DRUM -> new Drum(argument);
+            case TUBE -> new Tube(argument);
+        };
+    }
+}
---
+++ Lesson11/src/main/java/task1_musical_instrument/model/Tube.java
@@ -0,0 +1,15 @@
+package task1_musical_instrument.model;
+
+public class Tube implements IInstrument {
+    private int tubeDiameter;
+
+    public Tube(int tubeDiameter) {
+        this.tubeDiameter = tubeDiameter;
+    }
+
+    @Override
+    public void play() {
+        System.out.printf("играет труба диаметром %d см\n", tubeDiameter);
+    }
+
+}
