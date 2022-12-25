e0a6911db281f9d55c361e3b5dca9efe2772f4a0
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-12-26 00:23:04 +0300

    HW15 T2

--- Lesson15/src/main/java/hw1/Main.java
+++ Lesson15/src/main/java/hw1/Main.java
@@ -17,7 +17,6 @@ public class Main {
 
     public static void main(String[] args) {
         String stringWithPalindrome = readPalindromeFromFile(INPUT_FILE);
-
         write(stringWithPalindrome, OUTPUT_FILE);
     }
 
