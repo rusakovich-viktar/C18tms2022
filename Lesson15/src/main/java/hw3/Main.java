6f2899d95ec874f11033f73adf04b4c9b0bb8b2e
Viktar Rusakovich <mrisviz.rus@gmail.com>
2022-12-30 22:13:33 +0300

    HW15 T3

--- Lesson15/src/main/java/hw3/Main.java
+++ Lesson15/src/main/java/hw3/Main.java
@@ -1,5 +1,12 @@
 package hw3;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // * 3) Проверка на цензуру:
 //         * Создаете 2 файла.
 //         * 1 - й. Содержит исходный текст.
@@ -11,4 +18,58 @@ package hw3;
 //         * Если нет, то выводите соответствующее сообщение, кол-во предложений не
 //         прошедших проверку и сами предложения подлежащие исправлению.
 public class Main {
+
+    private static final String BLACKLIST = "Lesson15/src/main/java/hw3/blacklist.txt";
+    private static final String BLACKLIST_SEPARATOR = "[;\n]";
+    private static final String TEXT = "Lesson15/src/main/java/hw3/text.txt";
+
+
+    public static void main(String[] args) {
+
+        try {
+            String text = Files.readString(Path.of(TEXT));
+            String[] blacklist = Files.readString(Path.of(BLACKLIST)).split(BLACKLIST_SEPARATOR);
+            if (hasBlacklistWordInString(text, blacklist)) {
+                System.out.println("В тексте обнаружены слова из чёрного списка." +
+                        "\nПредложения, подлежащие исправлению:");
+                List<String> sentences = getSentencesFromText(text);
+                int sentencesWithBLWords = 0;
+                for (String sentence : sentences) {
+                    if (hasBlacklistWordInString(sentence, blacklist)) {
+                        System.out.println(sentence);
+                        sentencesWithBLWords++;
+                    }
+                }
+                System.out.println("Всего предложений, подлежащих исправлению: " + sentencesWithBLWords);
+            } else {
+                System.out.println("В тексте не встечаются слова из чёрного списка.");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    public static boolean hasBlacklistWordInString(String string, String[] blacklist) {
+        for (String blWord : blacklist) {
+            if (string.contains(blWord)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<String> getSentencesFromText(String text) {
+        List<String> originalSentences = Arrays.asList(text.split("[!.?]\\s*"));
+
+        List<String> result = new ArrayList<>();
+//        Pattern midWordHyphen = Pattern.compile(MID_WORD_HYPHEN);
+        for (String sentence : originalSentences) {
+            String replacedSentence = sentence.replaceAll("-*\n+", "");
+            result.add(replacedSentence);
+//            Matcher mSentence = midWordHyphen.matcher(sentence);
+//            result.add(mSentence.replaceAll(""));
+        }
+        return result;
+    }
 }
---
+++ TestModule/src/main/java/HelpClass.java
@@ -0,0 +1,12 @@
+public class HelpClass {
+    public static void main(String[] args) {
+
+
+        //Классы утил
+//        Arrays;
+//        Collections;
+//        Files.write(Path.of());
+
+    }
+    
+}
