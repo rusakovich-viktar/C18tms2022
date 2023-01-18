91c08d112a40337bdd4d0d8e1877246103bde645
Viktar Rusakovich <mrisviz.rus@gmail.com>
2023-01-18 12:18:25 +0300

    L21 JDBC

---
+++ Lesson21JDBC/src/main/java/by/tms/model/Student.java
@@ -0,0 +1,23 @@
+package by.tms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    private int id;
+    private String name;
+    private String surname;
+    private int course;
+
+    public Student(String name, String surname, int course) {
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+    }
+}
